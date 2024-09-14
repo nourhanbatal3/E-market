50552422c1b48904c6f2980b2de6ebd7d45a25c2
nourhan <nourhanbatal8@gmail.com>
2024-09-14 15:39:53 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const express = require("express");  
+const mongoose = require("mongoose"); 
+const path = require('path'); 
+
+const app = express(); 
+app.use(express.json()); 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+/*MongoDB connection
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => {
+    console.log("Connected to MongoDB successfully");
+  })
+  .catch((error) => {
+    console.log("Error with connecting to MongoDB:", error);
+  }); */
+
+
+const PORT = process.env.PORT || 3000; 
+
+
+app.get("/home", (req, res) => {
+  res.render("main"); 
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
